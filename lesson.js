3ebeaa7b618892dc3e7e18924dcb376b617fabe9
S.I Oloyede <officialsalau@gmail.com>
2020-02-17 23:27:39 +0100

    First commit

---
+++ lesson.js
@@ -0,0 +1,27 @@
+//Test JavaScipt
+alert ("Relax Oloyede is a cool JavaScript developer!");
+
+
+//For Button
+function myBotton(){
+    alert("This is fun and i love JavaScript too!");
+}
+
+
+//Purchase an AXE Perfume from DEKE Stores
+var price = 1000; //Price per 1 AXE
+var money = 5000; //How much money i got
+
+if (money >= price){
+    console.log("Buy the item.");
+} else {
+    console.log("Don't buy the item.");
+}
+
+
+//While loop
+var x =  1;
+while (x <= 10){
+    console.log(x + "y");
+    x +=1;
+}
